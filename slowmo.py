061b1969a0e14c4234f59c194aae030daef1769d
Yuquan Shan <yuquan.shan@yuquans-mbp.almaden.ibm.com>
2017-06-01 14:52:40 -0700

    Update slowmo.py

--- slowmo.py
+++ slowmo.py
@@ -5,42 +5,31 @@ import sys, datetime, pymongo, time
 
 # TODO: need to assert that the protocol is 1
 
-usage = "./slowmo.py <host_ip> <host_port> <time_span(min)> \
+usage = "./slowmo.py <host_ip> <host_port> <rs_id> <time_span(min)> \
 <sampling_interval(s)> <output_file>\n"
 
-def main(ip, port, span, it, output):
+def main(ip, port, rsid, span, it, output):
     client = pymongo.MongoClient(ip, int(port))
     span = 60*int(span) # calculate the total monitor time in seconds
     it = int(it)
-    mm = client.admin.command("replSetGetStatus")['members']
-    n = len(mm)     # the number of replicas in the set
+    rsinfo = client['local']['system.replset']  # find the collection
+                                                # containing the replset info
     box = []
     for i in range(0, span/it):
-        mm = client.admin.command("replSetGetStatus")['members']
-        box.append(get_lag(mm))
+        mm = rsinfo.find_one( { '_id' : rsid } )['members']
+        s = ''
+        for m in mm:
+            s = s + str(m['slaveDelay']) + ' '
+        box.append(s)
         time.sleep(it)
     fo = open(output, 'w')
     for i in box:
         fo.write(i+'\n')
     fo.close()
 
-def get_lag(mm):    # return the replication lag of replicas as well as prim id
-    n = len(mm)
-    prim = -1
-    for j in range(0,n):    # locate the primary
-        if mm[j]['stateStr'] == 'PRIMARY':
-            prim = j
-            primt = mm[j]['optimeDate']
-            break
-    assert prim >= 0, "LOST PRIMARY, MONITOR STOPS..."
-    s = ""
-    for j in range(0,n):
-        s = s + str((primt - mm[j]['optimeDate']).total_seconds()) + ' '
-    s = s + str(prim)
-    return s
-
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
+    if len(sys.argv) < 7:
         print usage
     else:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]\
+        sys.argv[6])
