39261befceb6376acd76b2c4813d355fa328b15f
Yuquan Shan <yuquan.shan@Yuquans-MacBook-Pro.local>
2017-06-10 21:57:42 -0700

    Modify data_factory/plot_the_latency.py

--- data_factory/plot_the_latency.py
+++ data_factory/plot_the_latency.py
@@ -22,19 +22,35 @@ def main(it, span, title, fnames):
         fi.close()
         n = span*60/it
         count = 0
+        nonsensepoints = 0  # we call a point nonsense if the latency diff from
+                            # the last sample is larger than the sampling
+                            # interval
+        nonsensebox = []
+        last = float(lines[0].split()[0])
         for l in lines:
             count = count + 1
             if count > n:
                 break;
             a = l.split()
             if len(a) > 0:
-                box.append(float(a[0]))
+                tmp = float(a[0])
+                box.append(tmp)
+                #if tmp - last <= it:
+                #    box.append(tmp)
+                #    last = tmp
+                #else:
+                #    box.append(last)
+                #    nonsensepoints = nonsensepoints + 1 # record the nonsense
+                #    nonsensebox.append(count)
         if max(box) > maxheight:
             maxheight = max(box)
+        print "the average lag of {} is {}\n".format(fname, sum(box)/len(box))
         plt.plot(np.linspace(it,it*len(box),len(box)), np.array(box), \
         '.--', color=colorpool[fc], label='replica '+str(fc+1))
         fc = fc + 1
 
+    #print "there are {} nonsense points, they are {}".format(nonsensepoints, \
+    #nonsensebox)
     plt.legend(loc = 'upper right')
     plt.xlabel('sec')
     plt.ylabel('replication delay (sec)')
